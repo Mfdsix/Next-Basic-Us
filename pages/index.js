46f25832f7876a843680f9a905a3c2c60faf13a2
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-02-19 20:34:32 +0700

    add navigation use next link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,34 @@
+import Link from 'next/link'
+
 function Index(){
-  return "Hello, this is is my index page"
+  return(
+  <div>
+    <h3>Hello, this is is my index page</h3>
+    <p>you can use below navigations as example</p>
+    <ul>
+      <li>
+        <Link as="/animal/mammal/elephant" href="/animal/[type]/[name]">
+        <a>Mammal - Elephant</a>
+        </Link>
+      </li>
+      <li>
+        <Link as="/animal/mammal/panda" href="/animal/[type]/[name]">
+        <a>Mammal - Panda</a>
+        </Link>
+      </li>
+      <li>
+        <Link as="/animal/reptile/snake" href="/animal/[type]/[name]">
+        <a>Mammal - Snake</a>
+        </Link>
+      </li>
+      <li>
+        <Link as="/animal/reptile/crocodile" href="/animal/[type]/[name]">
+        <a>Mammal - Crocodile</a>
+        </Link>
+      </li>
+    </ul>
+  </div>
+  )
 }
 
 export default Index
